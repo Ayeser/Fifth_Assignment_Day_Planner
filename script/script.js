1a29c810d721db73818d225b7361ee7dd2cc8f33
Matthew Wester <MatthewWester@live.com>
2019-12-19 02:47:30 -0700

    Day Planner now functions and requires basic assignment requirements

--- script/script.js
+++ script/script.js
@@ -1,28 +1,69 @@
-// First need to load all currently saved localStorage into middle column
-// Have button click event ready so saves new entries into localStorage
-
-let flagPast = false;
-//above line will flip to change the attr of event block to class "past". also "present" and "future"
-
-let currentTime = moment().format('LT');
-let currentTimeHour = parseInt(currentTime.charAt(0));
+let currentTime = moment().hour();
 
 $(document).ready(function () {
     document.getElementById("currentDay").innerHTML = moment().format('MMMM Do YYYY');
-    document.getElementById("9AM").value = "This will have localStorage";
-    console.log(typeof(currentTimeHour));
-    console.log(currentTimeHour);
-})
 
-$(".saveBtn").on('click', function() {
-    console.log("This button is working");
-    var activity = document.getElementById("9AM").value;
-    localStorage.setItem("9AM", activity);
-})
-
-function saveActivity(time) { 
-    if (time>5 && time<12) {
-        localStorage.setItem(time + "activity", document.getElementById(time + 'AM').value);
+    // These if statements simply populate the middle column with event descriptions or filler text
+    if(localStorage.getItem("9activity") === null) {
+        localStorage.setItem("9activity", "Write event description here");
     }
-    console.log(moment().format('LT'));
+    document.getElementById("9").innerHTML = localStorage.getItem("9activity");
+
+if(localStorage.getItem("10activity") === null) {
+        localStorage.setItem("10activity", "Write event description here");
+    }
+    document.getElementById("10").innerHTML = localStorage.getItem("10activity");
+
+    if(localStorage.getItem("11activity") === null) {
+        localStorage.setItem("11activity", "Write event description here");
+    }
+    document.getElementById("11").innerHTML = localStorage.getItem("11activity");
+
+    if(localStorage.getItem("12activity") === null) {
+        localStorage.setItem("12activity", "Write event description here");
+    }
+    document.getElementById("12").innerHTML = localStorage.getItem("12activity");
+
+    if(localStorage.getItem("13activity") === null) {
+        localStorage.setItem("13activity", "Write event description here");
+    }
+    document.getElementById("13").innerHTML = localStorage.getItem("13activity");
+
+    if(localStorage.getItem("14activity") === null) {
+        localStorage.setItem("14activity", "Write event description here");
+    }
+    document.getElementById("14").innerHTML = localStorage.getItem("14activity");
+
+    if(localStorage.getItem("15activity") === null) {
+        localStorage.setItem("15activity", "Write event description here");
+    }
+    document.getElementById("15").innerHTML = localStorage.getItem("15activity");
+
+    if(localStorage.getItem("16activity") === null) {
+        localStorage.setItem("16activity", "Write event description here");
+    }
+    document.getElementById("16").innerHTML = localStorage.getItem("16activity");
+
+    if(localStorage.getItem("17activity") === null) {
+        localStorage.setItem("17activity", "Write event description here");
+    }
+    document.getElementById("17").innerHTML = localStorage.getItem("17activity");
+    
+    // The below function sets the color of the middle blocks based on the current time
+    $('.ppf').each(function () {
+        if (currentTime > Number(this.id)) {
+            console.log("This is past");
+            this.setAttribute("class", "past");
+        } else if (currentTime < Number(this.id)) {
+            console.log("This is future");
+            this.setAttribute("class", "future");
+        } else {
+            this.setAttribute ("class", "present");
+        }
+    })
+})
+
+//this function is called when the save button in the right column is clicked
+function saveActivity(time) { 
+        localStorage.setItem(time + "activity", document.getElementById(time).textContent);
 };
